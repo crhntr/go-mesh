a6f52ee9dcabf3ae34ae11128de839d8c7e209f5
crhntr <crhntr.com@gmail.com>
2017-03-29 17:23:16 -0700

    implemented ParsePharmacologicalActionSet

--- pa.go
+++ pa.go
@@ -1,5 +1,10 @@
 package gomesh
 
+import (
+	"encoding/xml"
+	"io"
+)
+
 type PharmacologicalAction struct {
 	UI   UI     `xml:"DescriptorReferredTo>DescriptorUI"`
 	Name string `xml:"DescriptorReferredTo>DescriptorName>String"`
@@ -10,4 +15,36 @@ type PharmacologicalAction struct {
 type Substance struct {
 	UI   UI     `xml:"RecordUI"`
 	Name string `xml:"RecordName>String"`
+}
+
+func ParsePharmacologicalActionSet(r io.Reader) (<-chan PharmacologicalAction, chan error) {
+	dec := xml.NewDecoder(r)
+	drc := make(chan PharmacologicalAction)
+	errc := make(chan error, 2)
+	go func() {
+		for {
+			tok, err := dec.Token()
+			if err != nil {
+				errc <- err
+				close(drc)
+				return
+			}
+
+			switch t := tok.(type) {
+			case xml.StartElement:
+				if t.Name.Local == "PharmacologicalAction" {
+					dr := PharmacologicalAction{}
+
+					if err := dec.DecodeElement(&dr, &t); err != nil {
+						errc <- err
+						close(drc)
+						return
+					}
+
+					drc <- dr
+				}
+			}
+		}
+	}()
+	return drc, errc
 }
