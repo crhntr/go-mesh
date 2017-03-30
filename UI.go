bd508ea42e9e8f18d6797da22082bc5428b2a994
crhntr <crhntr.com@gmail.com>
2017-03-29 18:25:54 -0700

    added UIType

--- UI.go
+++ UI.go
@@ -1,3 +1,27 @@
 package gomesh
 
 type UI string
+
+type UIType int
+
+const (
+	UnknownUI UIType = iota
+	ConceptUI
+	QualifierUI
+	TermUI
+	DescriptorUI
+	// RecordUI // C or D
+)
+
+func (ui UI) Type() UIType {
+	if uit, exists := map[byte]UIType{
+		'Q': QualifierUI,
+		'M': ConceptUI,
+		'T': TermUI,
+		'D': DescriptorUI,
+	}[(string(ui))[0]]; !exists {
+		return UnknownUI
+	} else {
+		return uit
+	}
+}
