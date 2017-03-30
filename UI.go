5a8fdff5e851f27db07141affb245d418306d8f3
crhntr <crhntr.com@gmail.com>
2017-03-29 18:58:11 -0700

    refactored UIType_Type

--- UI.go
+++ UI.go
@@ -5,23 +5,30 @@ type UI string
 type UIType int
 
 const (
-	UnknownUI UIType = iota
-	ConceptUI
-	QualifierUI
-	TermUI
-	DescriptorUI
+	UnknownUI    UIType = iota
+	DescriptorUI        // 'D'
+	QualifierUI         // 'Q'
+	TermUI              // 'T'
+	ConceptUI           // 'M'
 	// RecordUI // C or D
 )
 
 func (ui UI) Type() UIType {
-	if uit, exists := map[byte]UIType{
-		'Q': QualifierUI,
-		'M': ConceptUI,
-		'T': TermUI,
-		'D': DescriptorUI,
-	}[(string(ui))[0]]; !exists {
+	if len(ui) == 0 {
 		return UnknownUI
-	} else {
-		return uit
+	}
+	str := string(ui)
+
+	switch {
+	case str[0] == 'D', str[0] == '*' && str[1] == 'D':
+		return DescriptorUI
+	case str[0] == 'Q', str[0] == '*' && str[1] == 'Q':
+		return QualifierUI
+	case str[0] == 'T':
+		return TermUI
+	case str[0] == 'M':
+		return ConceptUI
+	default:
+		return UnknownUI
 	}
 }
