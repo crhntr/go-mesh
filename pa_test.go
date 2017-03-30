4143664257820aa5750dafee5d2c6f5d11ab6457
crhntr <crhntr.com@gmail.com>
2017-03-29 18:37:37 -0700

    added TestParsePharmacologicalActionSet

---
+++ pa_test.go
@@ -0,0 +1,36 @@
+package gomesh_test
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/crhntr/gomesh"
+)
+
+func TestParsePharmacologicalActionSet(t *testing.T) {
+	f, err := os.Open("testdata/pa2017.xml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	pac, errc := gomesh.ParsePharmacologicalActionSet(f)
+
+	count := 0
+	for {
+		select {
+		case pa := <-pac:
+			t.Logf("%d: %q", count, pa.UI)
+			count++
+		case err := <-errc:
+			t.Logf("count: %d", count)
+			if err == io.EOF {
+				close(errc)
+				return
+			}
+			t.Error(err)
+			close(errc)
+			return
+		}
+	}
+}
