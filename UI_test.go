6465c2a9f82077e8a570ce76246a0859dac36078
crhntr <crhntr.com@gmail.com>
2017-03-31 01:12:06 -0700

    added test for UI

---
+++ UI_test.go
@@ -0,0 +1,25 @@
+package gomesh_test
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/crhntr/gomesh"
+)
+
+func TestUI(t *testing.T) {
+	testXML := []byte(`<foo><SomeUI>C114158</SomeUI></foo>`)
+	type TStruct struct {
+		XMLName xml.Name  `xml:"foo"`
+		UI      gomesh.UI `xml:"SomeUI"`
+	}
+	v := TStruct{}
+	err := xml.Unmarshal(testXML, &v)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if v.UI == "" {
+		t.Error("could not parse UI")
+	}
+}
